20b0a5a0be10734d15070520c7c0d1061d818731
jx8819 <110969246+jx8819@users.noreply.github.com>
2022-08-29 12:38:01 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,14 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-master.url = "github:NixOS/nixpkgs";
+
+  };
+
+  outputs = { self, nixpkgs, unstable, nixpkgs-master }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
